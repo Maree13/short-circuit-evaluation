5cdb0ae559ee774bf339e44dd10405897873777c
Mariana Sarovic <sarovicmariana@gmail.com>
2024-05-03 12:27:09 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Book from './components/Book';
 
-function App() {
+const App = () => {
+  const [show, setShow] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => setShow(!show)}>Ukaz / skryj</button>
+      {show && <Book />}
     </div>
   );
-}
+};
 
 export default App;
+
+// js - Short circuit evaluation
+
+// const value = false;
+// const value2 = 'Harry';
+
+// ||
+
+// const result2 = value || value2;
+// console.log(result2);
+
+// && - ked je value = true a zaroven "Harry", vysledok bude "Harry". Ked value = false, vysledok je false. Miesto "Harry" tam mozeme napriklad mat text v odstavci. Ked je hodnota value false, tak text neni vidiet, ked true, tak sa text zobrazi. V result pak mozeme ten text zobrazit do stranky. Takze to moze fungovat ako taky prepinac.
+
+// const result1 = value && value2;
+// console.log(result1);
+
+// js
---
+++ src/components/Book.js
@@ -0,0 +1,13 @@
+import './Book.css';
+
+import React from 'react';
+
+const Book = () => {
+  return (
+    <div>
+      <h1>Nazev knihy</h1>
+    </div>
+  );
+};
+
+export default Book;
